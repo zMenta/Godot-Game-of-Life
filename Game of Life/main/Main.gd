b364a84c7cf35a1be738a8706ccc658cdc42393f
LucasBrentano <brentano.l@gmail.com>
2022-10-18 10:51:38 -0300

    tweaks and a new basic text on start

--- Game of Life/main/Main.gd
+++ Game of Life/main/Main.gd
@@ -8,11 +8,3 @@ func _ready() -> void:
 	interface.connect("speed_changed", map, "_on_SpeedSlider_value_change")
 	interface.connect("pause_mode_changed", map, "_on_PauseButton_value_changed")
 
-
-# func _process(_delta: float) -> void:
-# 	var pos_mouse := get_global_mouse_position()
-
-# 	var pos_x := round(pos_mouse.x/8)
-# 	var pos_y := round(pos_mouse.y/8)
-
-# 	print(str(pos_x) + " | " + str(pos_y))
